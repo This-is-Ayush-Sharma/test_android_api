58abe503c9e84d68f4927b8b5618a7956ba45639
This-is-Ayush-Sharma <thisisayush79@gmail.com>
2023-04-11 09:27:14 +0530

    code made

---
+++ routes/router.js
@@ -0,0 +1,36 @@
+const app = require('express')();
+
+app.get('/data',(req,res)=>{
+    return res.json([
+        {
+            color: "red",
+            value: "#f00"
+        },
+        {
+            color: "green",
+            value: "#0f0"
+        },
+        {
+            color: "blue",
+            value: "#00f"
+        },
+        {
+            color: "cyan",
+            value: "#0ff"
+        },
+        {
+            color: "magenta",
+            value: "#f0f"
+        },
+        {
+            color: "yellow",
+            value: "#ff0"
+        },
+        {
+            color: "black",
+            value: "#000"
+        }
+    ])
+});
+
+module.exports = app;
